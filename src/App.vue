9c70a304fa756b08e67704b87e25788d6e2d453d
Tagir <qizilqara@yandex.ru>
2025-08-15 15:58:03 +0500

    pilot

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+<div class="main">
+  <button @click="count--">-</button>
+  <div>{{ count }}</div>
+  <button @click="count++">+</button>
+</div>
+
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const count = ref(0)
+
+
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: row;
+}
+div {
+
+  margin: 10%;
+}
+</style>
