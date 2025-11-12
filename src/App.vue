2c30ed6b0ff4e9159b025fd049ccd770e1c29a5d
Tagir <qizilqara@yandex.ru>
2025-11-13 00:56:54 +0500

    second

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@ import {ref} from 'vue';
 
 const text = ref('новое')
 
+const show = ref(false)
+
 function changeText () {
-  text.value = ('обновленно')
-  show.value = !show
+  text.value = ('обновлено')
+  show.value = !show.value
 }
 
 const ssrHtml = '<h1> Заголовок с сервера <h1> '
 
 const goods = ['Верняя одежда', 'Футболки', 'Толстовки', 'Джинсы', 'Шорты' ]
 
-const show = (false)
 
 </script>
 
@@ -22,39 +23,30 @@ const show = (false)
   <div v-html="ssrHtml" ></div>
   <!-- v-on -->
   <div class="container">
-    <p v-on:click="changeText()">Событие {{ text }}</p>
+    <div v-on:click="changeText()">Событие клик {{ text }}</div>
+    <!-- v-show -->
+    <div v-show="show">{{ text }}</div>
+    <!-- v-if -->
+    <div v-if="show">{{ text }}</div>
+    <div v-else="false"></div>
   </div>
-  <!-- v-for -->
-  <div>
-    <ol>
-      <li v-for="good in goods" :key="good">{{ good }}</li>
-    </ol>
-  </div>
-  <!-- v-show -->
-  <div v-show="show"></div>
-  <!-- v-if -->
-  <button v-if="Math.random() > 20"> видно</button>
-  <div v-else="false">скрыто</div>
   <!-- v-bind -->
-  <div :style="{color: 'green'}"> Продукты </div>
+    <h2 :style="{color: 'green'}"> Продукты </h2>
+  <!-- v-for -->
+    <div>
+      <ol>
+        <li v-for="good in goods" :key="good">{{ good }}</li>
+      </ol>
+    </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   width: 100%;
+  color: red;
   background-color: beige;
 }
-p {
-  display: flex;
-  font-family: 'Courier New', Courier, monospace;
-  font-size: larger;
-  font-weight: 500;
-  color: cadetblue;
-  flex-direction: column;
-  margin: 10%;
-  width: 100%;
-  background-color: brown;
-}
+
 </style>
